b85d2a02ea8dfd2025bd4ad77943b4ae007786c3
fabaohs <fabiohenriquesf8@gmail.com>
2021-10-20 21:00:04 -0200

    Melhorias no JavaScript e no HTML

--- script.js
+++ script.js
@@ -2,74 +2,54 @@ var btn = document.querySelector('.inputs .btn');
 
 var radios = document.getElementsByName('opr');
 
+const divRes = document.querySelector('#res p');
+
 btn.addEventListener('click', operation);
 
-function operation () {
+function operation() {
 
-    for(var i = 0; i < radios.length; i++){
-        if (radios[0].checked){
-            fazSoma();
+    var tn1 = document.querySelector('.inputs #tn1');
+    var tn2 = document.querySelector('.inputs #tn2');
+
+    var n1 = Number(tn1.value);
+    var n2 = Number(tn2.value);
+
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[0].checked) {
+            fazSoma(n1, n2);
             break;
         }
-        if (radios[1].checked){
-            fazSub();
+        if (radios[1].checked) {
+            fazSub(n1, n2);
             break;
         }
-        if (radios[2].checked){
-            fazMult();
+        if (radios[2].checked) {
+            fazMult(n1, n2);
             break;
         }
-        if (radios[3].checked){
-            fazDiv();
+        if (radios[3].checked) {
+            fazDiv(n1, n2);
             break;
         }
     }
 }
 
-function fazSoma () {   
-    var tn1 = document.querySelector('.inputs #tn1');
-    var tn2 = document.querySelector('.inputs #tn2');
-    var divRes = document.querySelector('#res p')
-    
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    
+function fazSoma(n1, n2) {
     let soma = n1 + n2;
     divRes.innerHTML = `Resultado: ${soma}`;
 }
 
-function fazSub() {
-    var tn1 = document.querySelector('.inputs #tn1');
-    var tn2 = document.querySelector('.inputs #tn2');
-    
-    var divRes = document.querySelector('#res p')
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    
+function fazSub(n1, n2) {
     let subt = n1 - n2;
     divRes.innerHTML = `Resultado: ${subt}`;
 }
 
-function fazMult () {
-    var tn1 = document.querySelector('.inputs #tn1');
-    var tn2 = document.querySelector('.inputs #tn2');
-    var divRes = document.querySelector('#res p');
-    
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-    
+function fazMult(n1, n2) {
     let mult = n1 * n2;
     divRes.innerHTML = `Resultado: ${mult}`;
 }
 
-function fazDiv () {
-    var tn1 = document.querySelector('.inputs #tn1');
-    var tn2 = document.querySelector('.inputs #tn2');
-    var divRes = document.querySelector('#res p');
-
-    var n1 = Number(tn1.value);
-    var n2 = Number(tn2.value);
-
+function fazDiv(n1, n2) {
     let divisao = n1 / n2;
     divRes.innerHTML = `Resultado: ${divisao}`;
 }
